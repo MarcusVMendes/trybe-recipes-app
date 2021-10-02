d9e8a74816b7047fe1e50c228d2055635b761fdb
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-10-02 18:54:06 -0300

    Requisito 47 funcionando

--- src/pages/InProgressDrinks.jsx
+++ src/pages/InProgressDrinks.jsx
@@ -4,72 +4,66 @@ import PropTypes from 'prop-types';
 
 function InProgressDrinks({ match: { params: { id } } }) {
   const [data, setData] = useState([]);
-  const [ingredientList, setIngredientList] = useState([]);
+
   useEffect(() => {
     async function requestDrink() {
       const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
       const response = await fetch(`${url}${id}`);
       const { drinks } = await response.json();
 
-      setData(drinks);
-
-      const objeto = [];
-      const entries = Object.entries(drinks[0])
-        .filter((e) => e[0].includes('strIngredient'));
-      entries.forEach((entrie) => {
-        if (entrie[0].includes('strIngredient') && entrie[1] !== null && entrie[1] !== '') {
-          objeto.push(entrie[1]);
-        }
-      });
-      setIngredientList(objeto);
-      // console.log(entries)
-      console.log(drinks)
+      setData(drinks[0]);
     }
     requestDrink();
   }, [id]);
 
+  const ingredientsArray = [];
+  const measureArray = [];
+  Object.keys(data).forEach((key) => {
+    if (key.includes('strIngredient') && data[key]) {
+      ingredientsArray.push(data[key]);
+    }
+    if (key.includes('strMeasure') && data[key]) {
+      measureArray.push(data[key]);
+    }
+  });
+
+  function renderIngredients(array) {
+    return (
+      array.map((ingredient, index) => (
+        <li key={ ingredient } data-testid={ `${index}-ingredient-step` }>
+          { ingredient }
+          {' '}
+          { measureArray[index] }
+        </li>
+      ))
+    );
+  }
+
+  const { idDrink, strDrinkThumb, strDrink, strCategory } = data;
   return (
     <div>
-      {data.map((drink) => (
-        <div key={ drink.idDrink } className="recipe-in-progress-container">
-          <img
-            src={ drink.strDrinkThumb }
-            alt={ drink.strDrink }
-            data-testid="recipe-photo"
-          />
-          <h2 data-testid="recipe-title">{drink.strDrink}</h2>
-          <button type="button" data-testid="share-btn">Compartilhar</button>
-          <button type="button" data-testid="favorite-btn">Favoritar</button>
-          <h3 data-testid="recipe-category">{drink.strCategory}</h3>
-          {/* <p data-testid={ `${id}-ingredient-step` }>{ drink.strIngredient}</p> */}
-          <ul>
-            {ingredientList.map((ingredient) => (
-              <li key={ ingredient }>{ingredient}</li>
-            ))}
-          </ul>
-          <p data-testid="instructions">Instruções</p>
-          <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
-        </div>
-      ))}
+      <div key={ idDrink } className="recipe-in-progress-container">
+        <img
+          src={ strDrinkThumb }
+          alt={ strDrink }
+          data-testid="recipe-photo"
+        />
+        <h2 data-testid="recipe-title">{ strDrink }</h2>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <h3 data-testid="recipe-category">{strCategory}</h3>
+        <ul>
+          {renderIngredients(ingredientsArray)}
+        </ul>
+        <p data-testid="instructions">Instruções</p>
+        <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+      </div>
     </div>
   );
 }
-
-// InProgressDrinks.propType = {
-//   match: PropTypes.shape({
-//     id: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
 
 InProgressDrinks.propTypes = {
   match: PropTypes.objectOf(PropTypes.number),
 }.isRequired;
 
 export default InProgressDrinks;
-
-/*
-Lembrates:
-mudar o p do ingrediente para uma li se for o caso.
-checar qual é o index dinamico que o ingrediente espera.
-atentar para a explicação sobre o tamanho do atributo ingrediente no readme
-*/
--- src/pages/InProgressFoods.jsx
+++ src/pages/InProgressFoods.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function InProgressFoods() {
+function InProgressFoods({ match: { params: { id } } }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function requestFoods() {
+      const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+      const response = await fetch(`${url}${id}`);
+      const { meals } = await response.json();
+
+      setData(meals[0]);
+    }
+    requestFoods();
+  }, [id]);
+
+  const ingredientsArray = [];
+  const measureArray = [];
+  Object.keys(data).forEach((key) => {
+    if (key.includes('strIngredient') && data[key]) {
+      ingredientsArray.push(data[key]);
+    }
+    if (key.includes('strMeasure') && data[key]) {
+      measureArray.push(data[key]);
+    }
+  });
+
+  function renderIngredients(array) {
+    return (
+      array.map((ingredient, index) => (
+        <li key={ ingredient } data-testid={ `${index}-ingredient-step` }>
+          { ingredient }
+          {' '}
+          { measureArray[index] }
+        </li>
+      ))
+    );
+  }
+
+  const { idMeal, strMealThumb, strMeal, strCategory } = data;
   return (
     <div>
-      InProgressFoods
+      <div key={ idMeal } className="recipe-in-progress-container">
+        <img
+          src={ strMealThumb }
+          alt={ strMeal }
+          data-testid="recipe-photo"
+        />
+        <h2 data-testid="recipe-title">{ strMeal }</h2>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <h3 data-testid="recipe-category">{strCategory}</h3>
+        <ul>
+          {renderIngredients(ingredientsArray)}
+        </ul>
+        <p data-testid="instructions">Instruções</p>
+        <button type="button" data-testid="finish-recipe-btn">Finalizar Receita</button>
+      </div>
     </div>
   );
 }
+
+InProgressFoods.propTypes = {
+  match: PropTypes.objectOf(PropTypes.number),
+}.isRequired;
 
 export default InProgressFoods;
